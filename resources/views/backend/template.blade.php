2770e1813ad74c74190afa619fb54959712c2455
DESKTOP-2R8EDF9\Developers <fahrulrozi99321@gmail.com>
2022-11-19 17:55:26 +0700

    Update: Merapihkan script dashboard dan template

--- resources/views/backend/template.blade.php
+++ resources/views/backend/template.blade.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Toko Sugeng</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Toko Sugeng</title>
 
- @include('layoutbackend.css')
+@include('layoutbackend.css')
 
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
-  <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="backend/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-  </div>
+<div class="preloader flex-column justify-content-center align-items-center">
+<img class="animation__shake" src="backend/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+</div>
 
-  @include('layoutbackend.navbar')
+@include('layoutbackend.navbar')
 
-  @include('layoutbackend.sidebar')
+@include('layoutbackend.sidebar')
 
-  @yield('content')
+@yield('content')
 
-  @include('layoutbackend.footer')
+@include('layoutbackend.footer')
 
-  <aside class="control-sidebar control-sidebar-dark">
-  </aside>
+<aside class="control-sidebar control-sidebar-dark">
+</aside>
 </div>
 
 @include('layoutbackend.js')
